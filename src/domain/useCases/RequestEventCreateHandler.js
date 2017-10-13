fce60cc24b725f9ad30f31a966afb6899d7e1fa1
Aleksa <aleksa47@gmail.com>
2017-10-13 18:07:07 +0200

    + Request Event Create use case handler and spec

---
+++ src/domain/useCases/RequestEventCreateHandler.js
@@ -0,0 +1,51 @@
+class RequestEventCreateHandler {
+  constructor(eventRepository, userRepository) {
+    this.eventRepository = eventRepository;
+    this.userRepository = userRepository;
+  }
+
+  handle(requestMessage) {
+    const creator = this.getCreator(requestMessage.userId);
+    const returnedEvent = this.addEventToRepository(
+      creator,
+      requestMessage.eventInfo
+    );
+    this.updateCreator(creator, returnedEvent);
+    return returnedEvent;
+  }
+
+  addEventToRepository(creator, info) {
+    const event = this.createEvent(creator.id, info);
+    event.addToGuestList(creator);
+    return this.eventRepository.add(event);
+  }
+
+  createEvent(creatorId, info) {
+    return new Event(
+      0,
+      creatorId,
+      info.title,
+      info.description,
+      info.date,
+      info.location
+    );
+  }
+
+  getCreator(userId) {
+    const creator = this.userRepository.getById(userId);
+    if (!creator) throw new Error('EventCreatorNotFoundException');
+    return creator;
+  }
+
+  updateCreator(user, event) {
+    user.joinEvent(event);
+    try {
+      this.userRepository.update(user);
+    } catch (e) {
+      this.eventRepository.delete(event);
+      throw e;
+    }
+  }
+}
+
+module.exports = RequestEventCreateHandler;
---
+++ test/domain/useCases/RequestEventCreateHandler.spec.js
@@ -0,0 +1,87 @@
+describe('Request Create Event Handler', function() {
+  var requestEventCreateHandler,
+    eventRepository,
+    userRepository,
+    eventCreateRequestMessage;
+
+  const eventInfo = {
+    title: 'Some Event',
+    description: 'Just an event.',
+    date: new Date(),
+    location: 'Somewhere'
+  };
+
+  beforeEach(function() {
+    eventRepository = new EventRepository();
+    userRepository = new UserRepository();
+    eventCreateRequestMessage = {
+      userId: 123,
+      eventInfo: eventInfo
+    };
+    requestEventCreateHandler = new RequestEventCreateHandler(
+      eventRepository,
+      userRepository
+    );
+  });
+
+  it('creates a new event', function() {
+    const returnedEvent = requestEventCreateHandler.handle(
+      eventCreateRequestMessage
+    );
+    expect(eventInfo.title).toBe(returnedEvent.title);
+    expect(eventInfo.description).toBe(returnedEvent.description);
+    expect(eventInfo.date).toBe(returnedEvent.date);
+    expect(eventInfo.location).toBe(returnedEvent.location);
+  });
+
+  it('adds the new event to repository', function() {
+    const returnedEvent = requestEventCreateHandler.handle(
+      eventCreateRequestMessage
+    );
+    const savedEvent = eventRepository.getById(returnedEvent.id);
+    expect(returnedEvent).toEqual(savedEvent);
+  });
+
+  it(
+    "adds event to creator's list of joined events" +
+      ' and persists the changes',
+    function() {
+      const returnedEvent1 = requestEventCreateHandler.handle(
+        eventCreateRequestMessage
+      );
+      const returnedEvent2 = requestEventCreateHandler.handle(
+        eventCreateRequestMessage
+      );
+      const savedEvent1 = eventRepository.getById(returnedEvent1.id);
+      const savedEvent2 = eventRepository.getById(returnedEvent2.id);
+      const creator = userRepository.getById(eventCreateRequestMessage.userId);
+      expect(creator.eventsJoined).toEqual([savedEvent1, savedEvent2]);
+    }
+  );
+
+  it('rolls back the event repo if creator update fails', function() {
+    const returnedEvent1 = requestEventCreateHandler.handle(
+      eventCreateRequestMessage
+    );
+    spyOn(userRepository, 'update').and.callFake(function() {
+      throw new Error('ProblemUpdatingEntityException');
+    });
+    expect(function() {
+      const returnedEvent2 = requestEventCreateHandler.handle(
+        eventCreateRequestMessage
+      );
+    }).toThrowError();
+    expect(eventRepository.entities).toEqual([returnedEvent1]);
+  });
+
+  it('throws error if user (creator) not found', function() {
+    spyOn(userRepository, 'getById').and.callFake(function() {
+      return null;
+    });
+    expect(function() {
+      const returnedEvent = requestEventCreateHandler.handle(
+        eventCreateRequestMessage
+      );
+    }).toThrowError('EventCreatorNotFoundException');
+  });
+});
