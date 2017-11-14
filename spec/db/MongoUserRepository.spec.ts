19dba653c225b200c152b9b58ab5569af796fb3d
Aleksa <aleksa47@gmail.com>
2017-11-14 12:26:42 +0100

    + mongo user repo implementation and spec

---
+++ spec/db/MongoUserRepository.spec.ts
@@ -0,0 +1,133 @@
+import { Db, Collection } from 'mongodb';
+import { whenConnected } from '../../src/db/mongodb/mongodb.config';
+import UserRepository, {
+  toDomainUser
+} from '../../src/db/mongodb/MongoUserRepository';
+import Event from '../../src/domain/entities/Event';
+import User from '../../src/domain/entities/User';
+
+const jsonUsers = require('../../src/db/users.json');
+
+describe('Mongo Users Repository', () => {
+  var db: Db,
+    testUsers: Collection,
+    testGoogleProvider: Collection,
+    repoUsers: Array<User>,
+    userRepository: UserRepository;
+
+  beforeAll(done => {
+    whenConnected.then(database => {
+      db = database;
+      db
+        .createCollection('testUsers')
+        .then(collection => {
+          testUsers = collection;
+          return db.createCollection('testGoogleProvider');
+        })
+        .then(collection => {
+          testGoogleProvider = collection;
+          userRepository = new UserRepository(testUsers, {
+            google: testGoogleProvider
+          });
+          const whenAdded: Array<
+            Promise<User>
+          > = jsonUsers.users.map((userInfo: any) => {
+            return userRepository.add(Object.assign(new User(), userInfo));
+          });
+          Promise.all(whenAdded).then(users => {
+            repoUsers = users;
+            done();
+          });
+        });
+    });
+  });
+
+  it('can add user to users collection and auth info to provider collection(s)', done => {
+    testUsers
+      .find()
+      .toArray()
+      .then(dbUsers => {
+        Promise.all(
+          dbUsers.map(async dbUser => {
+            const authInfo = await testGoogleProvider.findOne({
+              _id: dbUser.authentication[0].providerInfoId
+            });
+            authInfo.provider = dbUser.authentication[0].providerName;
+            return toDomainUser(dbUser, [authInfo]);
+          })
+        ).then(dbUsers => {
+          repoUsers.forEach(repoUser => {
+            expect(dbUsers.find(dbUser => dbUser === repoUser)).not.toBeNull();
+          });
+          done();
+        });
+      });
+  });
+
+  it('can get user by id or return null if not found', done => {
+    userRepository
+      .getById(0)
+      .then(user => {
+        expect(user).toBeNull();
+        return userRepository.getById(repoUsers[0].id);
+      })
+      .then(user => {
+        expect(user).toEqual(repoUsers[0]);
+        done();
+      });
+  });
+
+  it('can get user by authentication provider and id', done => {
+    userRepository
+      .getByAuthProviderId(
+        repoUsers[0].authenticationInfo[0].provider,
+        repoUsers[0].authenticationInfo[0].id
+      )
+      .then(user => {
+        expect(user).toEqual(repoUsers[0]);
+        done();
+      });
+  });
+
+  it("can update user's eventsJoined list", done => {
+    const event = new Event(
+      11,
+      22,
+      'Some Event',
+      'Description...',
+      new Date(),
+      'Nowhere'
+    );
+    userRepository
+      .getById(repoUsers[0].id)
+      .then(user => {
+        expect(user.eventsJoined.indexOf(event.id)).toBe(-1);
+        user.joinEvent(event);
+        return userRepository.updateEventsJoined(user);
+      })
+      .then(user => {
+        expect(repoUsers[0]).not.toEqual(user);
+        expect(user.eventsJoined.indexOf(event.id)).toBeGreaterThanOrEqual(0);
+        testUsers.findOne({ _id: user.id }).then(dbUser => {
+          Promise.all(
+            dbUser.authentication.map(async (authDbInfo: any) => {
+              const authInfo = await testGoogleProvider.findOne({
+                _id: authDbInfo.providerInfoId
+              });
+              authInfo.provider = authDbInfo.providerName;
+              return authInfo;
+            })
+          ).then(dbAuth => {
+            expect(toDomainUser(dbUser, dbAuth)).toEqual(user);
+            done();
+          });
+        });
+      });
+  });
+
+  afterAll(done => {
+    Promise.all([testUsers.drop(), testGoogleProvider.drop()]).then(() => {
+      done();
+    });
+  });
+});
---
+++ src/db/mongodb/MongoUserRepository.ts
@@ -0,0 +1,110 @@
+import UserRepository from '../../domain/repositories/UserRepository';
+import User from '../../domain/entities/User';
+
+import { Collection } from 'mongodb';
+
+interface authProviderCollections {
+  [key: string]: Collection;
+  local?: Collection;
+  google: Collection;
+  facebook?: Collection;
+  twitter?: Collection;
+}
+
+export default class MongoUserRepository implements UserRepository {
+  users: Collection;
+  authProviders: authProviderCollections;
+
+  constructor(users: Collection, authProviders: authProviderCollections) {
+    this.users = users;
+    this.authProviders = authProviders;
+  }
+
+  async add(user: User): Promise<User> {
+    delete user.id;
+    const authenticationInfo = user.authenticationInfo;
+    delete user.authenticationInfo;
+    const insertResult = await this.users.insertOne(user);
+    const userId = insertResult.ops[0]._id;
+
+    const authentication = await Promise.all(
+      authenticationInfo.map(async authInfo => {
+        const dbAuthInfo = Object.assign({ userId: userId }, authInfo);
+        delete dbAuthInfo.provider;
+        const result = await this.authProviders[authInfo.provider].insertOne(
+          dbAuthInfo
+        );
+        return {
+          providerName: authInfo.provider,
+          providerInfoId: result.ops[0]._id
+        };
+      })
+    );
+
+    this.users.updateOne(
+      { _id: userId },
+      { $set: { authentication: authentication } }
+    );
+
+    const domainUser = Object.assign(new User(), {
+      id: userId,
+      authenticationInfo: authenticationInfo
+    });
+
+    return domainUser;
+  }
+
+  async updateEventsJoined(user: User): Promise<User> {
+    const result = await this.users.findOneAndUpdate(
+      { _id: user.id },
+      { $set: { eventsJoined: user.eventsJoined } },
+      { returnOriginal: false }
+    );
+    if (!result.value) throw new Error('User update failed');
+    return toDomainUser(result.value, user.authenticationInfo);
+  }
+
+  async updateVerified(user: User): Promise<User> {
+    return null;
+  }
+
+  async findOne(query: any) {
+    return Promise.resolve(null);
+  }
+
+  async getById(id: Number): Promise<User> {
+    const dbUser = await this.users.findOne({ _id: id });
+    if (!dbUser) return null;
+    const dbAuth = await Promise.all(
+      dbUser.authentication.map(async (authDbInfo: any) => {
+        const authInfo = await this.authProviders[
+          authDbInfo.providerName
+        ].findOne({ _id: authDbInfo.providerInfoId });
+        authInfo.provider = authDbInfo.providerName;
+        return authInfo;
+      })
+    );
+    return toDomainUser(dbUser, dbAuth);
+  }
+
+  async getByAuthProviderId(provider: string, id: number): Promise<User> {
+    const authRecord = await this.authProviders[provider].findOne({ id: id });
+    return await this.getById(authRecord.userId);
+  }
+
+  async delete(user: User) {}
+}
+
+export function toDomainUser(dbUser: any, dbAuthInfo: any): User {
+  const domainUser = Object.assign(new User(), dbUser);
+  domainUser.id = domainUser._id;
+  delete domainUser._id;
+  delete domainUser.authentication;
+  domainUser.authenticationInfo = dbAuthInfo.map((providerInfo: any) => {
+    const authInfo = Object.assign({}, providerInfo);
+    delete authInfo._id;
+    delete authInfo.userId;
+    return authInfo;
+  });
+  return domainUser;
+}
